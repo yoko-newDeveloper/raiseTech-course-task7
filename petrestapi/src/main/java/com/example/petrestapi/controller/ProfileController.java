327bf5cc6a25dada36f3b5bc158cb9c05b791f16
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2023-06-28 21:00:25 +0900

    例外発生時のハンドリング実装したい

--- petrestapi/src/main/java/com/example/petrestapi/controller/ProfileController.java
+++ petrestapi/src/main/java/com/example/petrestapi/controller/ProfileController.java
@@ -2,13 +2,15 @@ package com.example.petrestapi.controller;
 
 import com.example.petrestapi.form.ProfileCreateForm;
 import com.example.petrestapi.form.ProfileUpdateForm;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -21,12 +23,7 @@ public class ProfileController {
     }
 
     @PostMapping("/profiles")
-    public ResponseEntity<Map<String, String>> createProfile(@RequestBody @Validated ProfileCreateForm form, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) { // バリデーションエラーをチェック
-            return ResponseEntity
-                    .badRequest() // Httpステータスコード400を返す
-                    .body(Map.of("error", bindingResult.getFieldError().getDefaultMessage()));
-        }
+    public ResponseEntity<Map<String, String>> createProfile(@RequestBody @Validated ProfileCreateForm form) {
 
         // バリデーションが成功
         URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
@@ -45,5 +42,18 @@ public class ProfileController {
     @DeleteMapping("/profiles/{id}")
     public ResponseEntity<Map<String, String>> deleteProfile(@PathVariable("id") int id) {
         return ResponseEntity.ok(Map.of("message", "profiles successfully deleted"));
+    }
+
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNoResourceFound(
+            ResourceNotFoundException e, HttpServletRequest request) {
+
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "path", request.getRequestURI());
+
+        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
     }
 }
--- petrestapi/src/main/java/com/example/petrestapi/form/NotBkank.java
+++
@@ -1,4 +0,0 @@
-package com.example.petrestapi.form;
-
-public @interface NotBkank {
-}
